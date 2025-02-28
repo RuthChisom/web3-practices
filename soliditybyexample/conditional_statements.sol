4c30e703ed98f9d51c76abe6ccc24e3e22f76ab2
Ruth Chisom <ndukwe100805043@gmail.com>
2025-02-28 18:13:38 +0100

    for and while loops

--- soliditybyexample/conditional_statements.sol
+++ soliditybyexample/conditional_statements.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-contract DaysSpentInAMonth{
-    function spent(uint256 day) public pure returns ( string memory)  {
+import "@openzeppelin/contracts/utils/Strings.sol"; // Import OpenZeppelin's Strings library
+
+contract ConditionalStatements{
+    //If/Else
+    function daysSpentInAMonth(uint256 day) public pure returns ( string memory)  {
         if (day < 10){
             return "The month is still young";
         } else if (day <=15) {
@@ -13,8 +16,30 @@ contract DaysSpentInAMonth{
     }
 
     //tenary method - my favorite method ...because this life no suppose hard
-    function spent2(uint256 y) public pure returns (string memory){
+    function daysSpentInAMonth2(uint256 y) public pure returns (string memory){
         return y < 20 ? "The month is still young" : "Sope Otilo!";
+    }
+
+    function happyBirthday(uint cake) public pure returns (string memory) {
+        // for loop
+        string memory message = "";
+
+        for (uint i = 1; i <= cake; i++) {
+            if (i == 5) {
+                return string(abi.encodePacked("Haba! You can't have more than 4 cakes in a day!! Come back tomorrow Ojukokoro!!!"));
+            }
+            message = string(abi.encodePacked(message, "Happy Birthday Champ! Here's your cake ", Strings.toString(i), "--- ||\n\n\n"));
+        }
+
+        return message;
+
+        // while loop
+        // uint j;
+        // while (j<5) {
+        //     j++;
+        // }
+        // return j;
+
     }
 }
 
