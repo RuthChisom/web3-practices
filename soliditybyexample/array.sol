6facfe0695d9622757e4734711b11aac7df6f2e1
Ruth Chisom <ndukwe100805043@gmail.com>
2025-03-14 13:21:45 +0100

    different ways to remove from an array

--- soliditybyexample/array.sol
+++ soliditybyexample/array.sol
@@ -45,4 +45,44 @@ contract Array {
         b[1][1] = 5;
         b[1][2] = 6;
     }
-}
+
+    // Remove an array element by shifting elements from right to left
+    uint256[] public arr;
+
+    function remove1(uint256 _index) public{
+        require(_index < arr.length, "index out of bounds");
+         for (uint256 i = _index; i<arr.length - 1; i++){
+            arr[i] = arr[i+1];
+         }
+         arr.pop();
+    }
+
+    function testShift() external {
+        arr = [10, 20, 30, 40, 50];
+        remove(2); //[10, 20, 40, 50]
+        assert(arr[0] == 10);
+        assert(arr[1] == 20);
+        assert(arr[2] == 40);
+        assert(arr[3] == 50);
+        assert(arr.length == 4);
+    }
+
+    // Remove an element by copying last element into the place to remove
+    function remove2(uint256 index) public {
+        arr[index] = arr[arr.length - 1];
+        arr.pop();
+    }
+
+    function testShift2() public {
+        arr = [11, 22, 33, 44];
+        remove2(1); //[11, 33, 44]
+        assert(arr.length == 3);
+        assert(arr[0] == 11);
+        assert(arr[1] == 33);
+        assert(arr[2] == 44);
+
+        remove2(2);//[11, 33]
+        assert(arr.length == 2);
+        assert(arr[0] == 11);
+        assert(arr[1] == 33);
+    }
